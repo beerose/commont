8a8d69e69896260c4c26a8acd36fa85bfacd86ef
Aleksandra <alexsandra.sikora@gmail.com>
2021-08-29 16:42:33 +0200

    New api, tests, and all

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,16 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export interface Comment {
-  /** The string provided by the sender */
   author: string;
   content: string;
-  created_at: string;
-  /**
-   * Needed for optimistic update.
-   * When the comment is submitted, it is `'sending'`.
-   * When the request succeeds and the comment is not hidden in the database, it turns into `'added'`.
-   * When the request succeeds and the comment is hidden and awaiting approval, we receive `'delivered-awaiting-approval'`.
-   * When the request fails, the status is `'failed'`. - You can use this information to prompt user to retry.
-   */
-  status?: CommentStatus;
+  topic: string;
+  createdAt: string;
+  status?: CommontStatus;
 }
 
-interface CommentInternal extends Comment {
-  post_id: string;
+interface InternalComment extends Comment {
+  hidden: boolean;
 }
 
 /**
@@ -27,26 +20,20 @@ interface CommentInternal extends Comment {
  * When the request succeeds and the comment is hidden and awaiting approval, we receive `'delivered-awaiting-approval'`.
  * When the request fails, the status is `'failed'`. - You can use this information to prompt user to retry.
  */
-export type CommentStatus =
+export type CommontStatus =
   | 'sending'
   | 'added'
   | 'delivered-awaiting-approval'
   | 'failed';
 
-const errorMessage =
-  'Oops! Fetching comments was unsuccessful. Try again later.';
-
-export interface UseCommentsError {
-  error: string;
-  details: string;
-}
-
-export interface UseCommentsConfig {
+export interface UseCommentsParameters {
+  projectId: string;
+  topic: string;
   take?: number;
   skip?: number;
 }
 
-export interface UseComentsResult {
+export interface UseCommentsResult {
   comments: Comment[];
   addComment: ({
     content,
@@ -55,61 +42,105 @@ export interface UseComentsResult {
   refetch: () => void;
   count: number;
   loading: boolean;
-  error: UseCommentsError | null;
+  error: string | null;
 }
 
-const URL = 'https://www.commont.app/api/';
+const _fetchComments = async (payload: Record<string, any>) => {
+  const params = new URLSearchParams(payload).toString();
+  const url = `https://www.commont.app/api/comments?${params}`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const responseJson: {
+    comments: Comment[];
+    count: number;
+  } = await response.json();
+
+  if (response.ok) {
+    return responseJson;
+  }
+
+  if (response.ok) {
+    return responseJson
+      ? responseJson
+      : Promise.reject(new Error('Empty API response'));
+  }
+
+  return Promise.reject(new Error(response.statusText));
+};
+
+const _addComment = async (payload: Record<string, string>) => {
+  const response = await fetch('https://www.commont.app/api/add-comment', {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const responseJson: { comment: InternalComment } = await response.json();
+
+  if (response.ok) {
+    return responseJson
+      ? responseJson
+      : Promise.reject(new Error('Empty API response'));
+  }
+
+  return Promise.reject(new Error(response.statusText));
+};
 
 /**
  * Fetches comments from Hasura backend specified in `hasuraUrl` on mount and whenever
  * `config.take` or `config.skip` change.
  *
  * @param projectId Id of your Commont's project
- * @param postId Comments will be fetched for the post with id `postId`
+ * @param topic Comments will be fetched for the post with id `topic`
  * @param config Configurable skip and take for the GraphQL query to Hasura
  * @returns comments for given post, aggregated count of all comments, error,
  *          loading state and a function to refetch data from backend.
  */
-export const useComments = (
-  projectId: string,
-  postId: string,
-  config?: UseCommentsConfig
-): UseComentsResult => {
+export const useComments = ({
+  projectId,
+  topic,
+  skip,
+  take,
+}: UseCommentsParameters): UseCommentsResult => {
   const [comments, setComments] = useState<Comment[]>([]);
   const [count, setCount] = useState(0);
-  const [error, setError] = useState<UseCommentsError | null>(null);
+  const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
 
+  const isMounted = useIsMounted();
+
   const fetchComments = () => {
-    setLoading(true);
-    fetch(URL + 'comments', {
-      method: 'POST',
-      body: JSON.stringify({
-        projectId,
-        postId,
-        ...(config?.take && { take: config.take }),
-        ...(config?.skip && { skip: config.skip }),
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    if (isMounted) {
+      setLoading(true);
+    }
+    _fetchComments({
+      projectId,
+      topic,
+      ...(take && { take }),
+      ...(skip && { skip }),
     })
-      .then(res => res.json())
       .then(res => {
-        setComments(res.comments);
-        setCount(res.count);
-        setLoading(false);
+        if (isMounted) {
+          setComments(res.comments);
+          setCount(res.count);
+          setLoading(false);
+        }
       })
-      .catch(err => {
-        setError({
-          error: errorMessage,
-          details: err,
-        });
-        setLoading(false);
+      .catch((err: Error) => {
+        if (isMounted) {
+          setError(err.message);
+          setLoading(false);
+        }
       });
   };
 
-  useEffect(fetchComments, [config?.take, config?.skip]);
+  useEffect(fetchComments, [take, skip]);
 
   const addComment = ({
     content,
@@ -117,58 +148,57 @@ export const useComments = (
   }: Pick<Comment, 'content' | 'author'>) => {
     const createdAt = new Date().toDateString();
 
-    const newComment: CommentInternal = {
+    const newComment: Comment = {
       author,
       content,
-      post_id: postId,
-      created_at: createdAt,
+      topic,
+      createdAt,
       status: 'sending',
     };
-    setComments(prev => [newComment, ...prev]);
-    setCount(prev => ++prev);
+    if (isMounted) {
+      setComments(prev => [newComment, ...prev]);
+      setCount(prev => ++prev);
+    }
 
-    fetch(URL + 'addComment', {
-      method: 'POST',
-      body: JSON.stringify({
-        projectId,
-        postId,
-        content,
-        author,
-      }),
+    _addComment({
+      projectId,
+      topic,
+      content,
+      author,
     })
-      .then(res => res.json())
       .then(res => {
-        const remoteComment = res.data.insert_comments_one;
-        setComments(prev =>
-          prev.map(
-            (x): Comment =>
-              x === newComment
-                ? {
-                    ...remoteComment,
-                    status: remoteComment.hidden
-                      ? 'delivered-awaiting-approval'
-                      : 'added',
-                  }
-                : x
-          )
-        );
+        const remoteComment = res.comment;
+        if (isMounted) {
+          setComments(prev =>
+            prev.map(
+              (x): Comment =>
+                x === newComment
+                  ? {
+                      ...remoteComment,
+                      status: remoteComment.hidden
+                        ? 'delivered-awaiting-approval'
+                        : 'added',
+                    }
+                  : x
+            )
+          );
+        }
       })
-      .catch(err => {
-        setError({
-          error: errorMessage,
-          details: err,
-        });
-        setComments(prev =>
-          prev.map(
-            (x): Comment =>
-              x === newComment
-                ? {
-                    ...newComment,
-                    status: 'failed',
-                  }
-                : x
-          )
-        );
+      .catch((err: Error) => {
+        if (isMounted) {
+          setError(err.message);
+          setComments(prev =>
+            prev.map(
+              (x): Comment =>
+                x === newComment
+                  ? {
+                      ...newComment,
+                      status: 'failed',
+                    }
+                  : x
+            )
+          );
+        }
       });
   };
 
@@ -180,4 +210,14 @@ export const useComments = (
     loading,
     error,
   };
+};
+
+const useIsMounted = () => {
+  const isMounted = useRef(true);
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  });
+  return isMounted;
 };
